04860461d3bf04036969db0f9b4cc2bffbcb3e83
Kyunghwan Kwon <kwon@toanyone.net>
2021-07-25 19:46:02 +0900

    Add comment on parser api

--- include/cbor/parser.h
+++ include/cbor/parser.h
@@ -15,6 +15,16 @@ typedef struct {
 
 cbor_error_t cbor_parser_init(cbor_parser_t *parser,
 		const void *msg, size_t msgsize);
+/**
+ * Parse the encoded CBOR messages into items.
+ *
+ * @param[in,out] parser instance to keep the context
+ * @param[out] items a pointer to item buffers
+ * @param[in] maxitems the maximum number of items to be stored in @p items
+ * @param[out] items_parsed the number of items parsed gets stored if not null
+ *
+ * @return a code of @ref cbor_error_t
+ */
 cbor_error_t cbor_parse(cbor_parser_t *parser,
 		cbor_item_t *items, size_t maxitems, size_t *items_parsed);
 
