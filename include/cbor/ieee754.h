cc0a60ee68bc3ad91e69d61625d4a54f98e9e258
Kyunghwan Kwon <kwon@toanyone.net>
2021-07-28 14:40:06 +0900

    add function prototypes for float and test cases

--- include/cbor/ieee754.h
+++ include/cbor/ieee754.h
@@ -7,9 +7,42 @@ extern "C" {
 
 #include <stdint.h>
 
-float ieee754_convert_half_to_single(uint16_t ihalf);
-uint16_t ieee754_convert_single_to_half(float fsingle);
-uint16_t ieee754_convert_double_to_half(double fdouble);
+typedef union {
+	uint16_t value;
+	struct {
+		uint32_t m: 10;
+		uint32_t e: 5;
+		uint32_t sign: 1;
+	} components;
+} ieee754_half_t;
+
+typedef union {
+	float value;
+	struct {
+		uint32_t m: 23;
+		uint32_t e: 8;
+		uint32_t sign: 1;
+	} components;
+} ieee754_single_t;
+
+typedef union {
+	double value;
+	struct {
+		uint64_t m: 52;
+		uint64_t e: 11;
+		uint64_t sign: 1;
+	} components;
+} ieee754_double_t;
+
+typedef union {
+	ieee754_half_t h;
+	ieee754_single_t s;
+	ieee754_double_t d;
+} ieee754_t;
+
+ieee754_single_t ieee754_convert_half_to_single(uint16_t value);
+ieee754_half_t ieee754_convert_single_to_half(float value);
+ieee754_half_t ieee754_convert_double_to_half(double value);
 
 #if defined(__cplusplus)
 }
--- tests/src/ieee754_test.cpp
+++ tests/src/ieee754_test.cpp
@@ -13,5 +13,26 @@ TEST_GROUP(IEEE754) {
 	}
 };
 
-TEST(IEEE754, t) {
+TEST(IEEE754, ShouldConvertToHalf_WhenSinglePrecisionZeroGiven) {
+}
+TEST(IEEE754, ShouldConvertToHalf_WhenSinglePrecisionIndefinteGiven) {
+}
+TEST(IEEE754, ShouldConvertToHalf_WhenSinglePrecisionNaNGiven) {
+}
+TEST(IEEE754, ShouldConvertToHalf_WhenHalfPrecisionSingleValueGiven) {
+}
+TEST(IEEE754, ShouldKeepSingle_WhenSinglePrecisionGiven) {
+}
+
+TEST(IEEE754, ShouldConvertToHalf_WhenDoublePrecisionZeroGiven) {
+}
+TEST(IEEE754, ShouldConvertToHalf_WhenDoublePrecisionIndefinteGiven) {
+}
+TEST(IEEE754, ShouldConvertToHalf_WhenDoublePrecisionNaNGiven) {
+}
+TEST(IEEE754, ShouldConvertToHalf_WhenHalfPrecisionDoubleValueGiven) {
+}
+TEST(IEEE754, ShouldConvertToSingle_WhenSinglePrecisionDoubleValueGiven) {
+}
+TEST(IEEE754, ShouldKeepDouble_WhenDoublePrecisionGiven) {
 }
